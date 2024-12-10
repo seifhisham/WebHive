93f8ef08cb59075135fbc00fbb313fc5016a8185
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2024-12-10 22:25:41 +0200

    Update route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -33,6 +33,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://marshal-ui-yt.vercel.app/"
+      : "https://web-hive.vercel.app/"
   );
 }
